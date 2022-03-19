bc666257be2614109f68210c88ff29b7b99790f2
cze <cze_14@outlook.com>
2022-03-19 14:04:11 +0300

    ejs added

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
 const express = require('express');
+
+
 const app = express();
 
+// template engine
+
+app.set("view engine", "ejs")
 
 
+//middlewares
+app.use(express.static("public"))
+
+
+// routes 
 app.get('/', (req,res) => {
-    res.send('index page')
+    res.status(200).render('index', {
+      page_name: 'index'
+    })
+})
+app.get('/about', (req,res) => {
+    res.status(200).render('about', {
+      page_name: 'about'
+    })
 })
 
 
