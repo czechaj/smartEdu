ebb352c849af2fad674591dd0141fc68854a31cb
cze <cze_14@outlook.com>
2022-03-20 13:54:38 +0300

    category schema created, category routes anf references added,

--- app.js
+++ app.js
@@ -1,29 +1,32 @@
 const express = require('express');
-
-
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const pageRoute = require('./routes/pageRoute');
+const courseRoute = require('./routes/courseRoute');
+const categoryRoute = require('./routes/categoryRoute');
 const app = express();
+
+// connect db
+
+mongoose.connect('mongodb://localhost/smartedu-db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+  })
+.then(() => {console.log("db connected");});
 
 // template engine
 
-app.set("view engine", "ejs")
-
+app.set('view engine', 'ejs');
 
 //middlewares
-app.use(express.static("public"))
+app.use(express.static('public'));
+app.use(bodyParser.json()) // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
-
-// routes 
-app.get('/', (req,res) => {
-    res.status(200).render('index', {
-      page_name: 'index'
-    })
-})
-app.get('/about', (req,res) => {
-    res.status(200).render('about', {
-      page_name: 'about'
-    })
-})
-
+// routes
+app.use('/', pageRoute);
+app.use('/courses', courseRoute);
+app.use('/categories', categoryRoute)
 
 
 const port = 3000;
